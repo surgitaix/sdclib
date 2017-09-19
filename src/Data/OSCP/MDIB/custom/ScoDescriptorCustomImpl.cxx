bb947f7997690b2061826d997bf18c4ff13a3b79
Sebastian Bürger <buerger@surgitaix.com>
2017-09-19 14:13:52 +0200

    adapting customImpls: ScoDescriptor

--- src/Data/OSCP/MDIB/custom/ScoDescriptorCustomImpl.cxx
+++ src/Data/OSCP/MDIB/custom/ScoDescriptorCustomImpl.cxx
@@ -4,8 +4,8 @@
 #include "OSCLib/Data/OSCP/MDIB/ScoDescriptor.h"
 #include "OSCLib/Data/OSCP/MDIB/SetAlertStateOperationDescriptor.h"
 #include "OSCLib/Data/OSCP/MDIB/SetContextStateOperationDescriptor.h"
-//??
-#include "OSCLib/Data/OSCP/MDIB/SetRangeOperationDescriptor.h"
+//?? -> TODO: discuss with standardizing group
+//#include "OSCLib/Data/OSCP/MDIB/SetRangeOperationDescriptor.h"
 #include "OSCLib/Data/OSCP/MDIB/SetStringOperationDescriptor.h"
 #include "OSCLib/Data/OSCP/MDIB/SetValueOperationDescriptor.h"
 
@@ -16,8 +16,8 @@ namespace Data {
 namespace OSCP {
 
 template <class WrapperOperationDescriptorType>
-void SCODescriptor::collectOperationDescriptorImpl(std::vector<WrapperOperationDescriptorType> & out) const {
-	const CDM::SCODescriptor & sco(*this->data);
+void ScoDescriptor::collectOperationDescriptorImpl(std::vector<WrapperOperationDescriptorType> & out) const {
+	const CDM::ScoDescriptor & sco(*this->data);
 	for (const auto & operation : sco.Operation()) {
 		if (const auto foundDescriptor = dynamic_cast<const typename WrapperOperationDescriptorType::WrappedType *>(&operation)) {
 			out.push_back(ConvertFromCDM::convert(*foundDescriptor));
@@ -25,37 +25,37 @@ void SCODescriptor::collectOperationDescriptorImpl(std::vector<WrapperOperationD
 	}
 }
 
-std::vector<ActivateOperationDescriptor> SCODescriptor::collectAllActivateOperationDescriptors() const {
+std::vector<ActivateOperationDescriptor> ScoDescriptor::collectAllActivateOperationDescriptors() const {
 	std::vector<ActivateOperationDescriptor> result;
 	collectOperationDescriptorImpl<ActivateOperationDescriptor>(result);
 	return result;
 }
 
-std::vector<SetAlertStateOperationDescriptor> SCODescriptor::collectAllSetAlertStateOperationDescriptors() const {
+std::vector<SetAlertStateOperationDescriptor> ScoDescriptor::collectAllSetAlertStateOperationDescriptors() const {
 	std::vector<SetAlertStateOperationDescriptor> result;
 	collectOperationDescriptorImpl<SetAlertStateOperationDescriptor>(result);
 	return result;
 }
 
-std::vector<SetContextStateOperationDescriptor> SCODescriptor::collectAllSetContextOperationDescriptors() const {
+std::vector<SetContextStateOperationDescriptor> ScoDescriptor::collectAllSetContextOperationDescriptors() const {
 	std::vector<SetContextStateOperationDescriptor> result;
 	collectOperationDescriptorImpl<SetContextStateOperationDescriptor>(result);
 	return result;
 }
 
-std::vector<SetRangeOperationDescriptor> SCODescriptor::collectAllSetRangeOperationDescriptors() const {
-	std::vector<SetRangeOperationDescriptor> result;
-	collectOperationDescriptorImpl<SetRangeOperationDescriptor>(result);
-	return result;
-}
+//std::vector<SetRangeOperationDescriptor> ScoDescriptor::collectAllSetRangeOperationDescriptors() const {
+//	std::vector<SetRangeOperationDescriptor> result;
+//	collectOperationDescriptorImpl<SetRangeOperationDescriptor>(result);
+//	return result;
+//}
 
-std::vector<SetStringOperationDescriptor> SCODescriptor::collectAllSetStringOperationDescriptors() const {
+std::vector<SetStringOperationDescriptor> ScoDescriptor::collectAllSetStringOperationDescriptors() const {
 	std::vector<SetStringOperationDescriptor> result;
 	collectOperationDescriptorImpl<SetStringOperationDescriptor>(result);
 	return result;
 }
 
-std::vector<SetValueOperationDescriptor> SCODescriptor::collectAllSetValueOperationDescriptors() const {
+std::vector<SetValueOperationDescriptor> ScoDescriptor::collectAllSetValueOperationDescriptors() const {
 	std::vector<SetValueOperationDescriptor> result;
 	collectOperationDescriptorImpl<SetValueOperationDescriptor>(result);
 	return result;
