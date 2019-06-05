36f017125c270dcb70d3f36692fefddcd62abb71
Sebastian Bürger <buerger@surgitaix.com>
2019-06-05 15:29:22 +0200

    dpws:Profile and dpws:DiscoveryType added to wsdls to conform standard

--- include/SDCLib/Data/SDC/WSDL/GetServiceWSDL.h
+++ include/SDCLib/Data/SDC/WSDL/GetServiceWSDL.h
@@ -3,7 +3,7 @@
 
 #include <string.h>
 
-const std::string getServiceWsdl=R"(<wsdl:definitions xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" targetNamespace="http://standards.ieee.org/downloads/11073/11073-20701-2018" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:tns="http://standards.ieee.org/downloads/11073/11073-20701-2018" xmlns:wsa="http://www.w3.org/2005/08/addressing" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:mm="http://standards.ieee.org/downloads/11073/11073-10207-2017/message">
+const std::string getServiceWsdl=R"(<wsdl:definitions xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" xmlns:wsp="http://www.w3.org/ns/ws-policy" targetNamespace="http://standards.ieee.org/downloads/11073/11073-20701-2018" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:tns="http://standards.ieee.org/downloads/11073/11073-20701-2018" xmlns:wsa="http://www.w3.org/2005/08/addressing" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:mm="http://standards.ieee.org/downloads/11073/11073-10207-2017/message" xmlns:dpws="http://docs.oasis-open.org/ws-dd/ns/dpws/2009/01">
     <wsdl:types>
         <xs:schema targetNamespace="http://standards.ieee.org/downloads/11073/11073-10207-2017/message" xmlns:xs="http://www.w3.org/2001/XMLSchema">
             <xs:include schemaLocation="BICEPS_MessageModel.xsd"/>
@@ -27,7 +27,7 @@ const std::string getServiceWsdl=R"(<wsdl:definitions xmlns:wsdl="http://schemas
     <wsdl:message name="GetMdStateResponse">
         <wsdl:part element="mm:GetMdStateResponse" name="parameters"/>
     </wsdl:message>
-    <wsdl:portType name="GetService">
+    <wsdl:portType name="GetService" dpws:DiscoveryType="p1:ServiceProvider">
         <wsdl:operation name="GetMdDescription">
             <wsdl:input message="tns:GetMdDescription" wsa:Action="http://standards.ieee.org/downloads/11073/11073-20701-2018/GetService/GetMdDescription"/>
             <wsdl:output message="tns:GetMdDescriptionResponse" wsa:Action="http://standards.ieee.org/downloads/11073/11073-20701-2018/GetService/GetMdDescriptionResponse"/>
@@ -40,6 +40,9 @@ const std::string getServiceWsdl=R"(<wsdl:definitions xmlns:wsdl="http://schemas
             <wsdl:input message="tns:GetMdState" wsa:Action="http://standards.ieee.org/downloads/11073/11073-20701-2018/GetService/GetMdState"/>
             <wsdl:output message="tns:GetMdStateResponse" wsa:Action="http://standards.ieee.org/downloads/11073/11073-20701-2018/GetService/GetMdStateResponse"/>
         </wsdl:operation>
+        <wsp:Policy>
+            <dpws:Profile wsp:Optional="true"/>
+        </wsp:Policy>
     </wsdl:portType>
     <wsdl:binding name="GetServiceBinding" type="tns:GetService" xmlns:p1="http://schemas.xmlsoap.org/wsdl/soap12/">
         <p1:binding style="document" transport="http://schemas.xmlsoap.org/soap/http"/>
