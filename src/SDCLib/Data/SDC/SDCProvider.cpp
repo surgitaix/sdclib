cbc1f5d7594e779327c12787500d5d57da971271
Georg Lorenz Rosenau <georg.lorenz.rosenau@gmail.com>
2019-07-11 12:11:33 +0200

    Fixed: SDC-1111 Chhnges to the AlertConditionState are now evaluated in SDCProvider::updateState

--- src/SDCLib/Data/SDC/SDCProvider.cpp
+++ src/SDCLib/Data/SDC/SDCProvider.cpp
@@ -660,6 +660,14 @@ void SDCProvider::updateState(const AlertSignalState & object) {
 
 void SDCProvider::updateState(const AlertConditionState & object) {
 	updateMDIB(object);
+	//Evaluate Alert Conditions sources, based on changes to the AlertCoditionState
+	std::string handle = object.getDescriptorHandle();
+	MdDescription mdDescription = getMdDescription();
+	std::unique_ptr<AlertConditionDescriptor> alertConditionDescription(std::move(mdDescription.findDescriptor<AlertConditionDescriptor>(handle)));
+	for (auto source : alertConditionDescription->getSourceList())
+	{
+		evaluateAlertConditions(source);
+	}
 	notifyAlertEventImpl(object);
 }
 
@@ -671,6 +679,14 @@ void SDCProvider::updateState(const EnumStringMetricState & object) {
 
 void SDCProvider::updateState(const LimitAlertConditionState & object) {
 	updateMDIB(object);
+	//Evaluate Alert Conditions sources, based on changes to the AlertCoditionState
+	std::string handle = object.getDescriptorHandle();
+	MdDescription mdDescription = getMdDescription();
+	std::unique_ptr<AlertConditionDescriptor> alertConditionDescription(std::move(mdDescription.findDescriptor<AlertConditionDescriptor>(handle)));
+	for (auto source : alertConditionDescription->getSourceList())
+	{
+		evaluateAlertConditions(source);
+	}
 	notifyAlertEventImpl(object);
 }
 
