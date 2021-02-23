24983d72ed5d0faf076b34176f6d98304244f058
Fabian Baumeister <baumeister@surgitaix.com>
2021-02-23 12:07:50 +0100

    Set a fixed version to xerces-c dependency checkout.

--- Dependencies/xerces-c/xerces_x64.bat
+++ Dependencies/xerces-c/xerces_x64.bat
@@ -3,7 +3,8 @@
 :: Clone xerces to defined folder
 SET XERCES_FOLDER=xerces-c
 
-git.exe clone https://github.com/apache/xerces-c.git %XERCES_FOLDER%
+:: Clone explicit version branch
+git.exe clone -b xerces-3.2.0 --depth 1 https://github.com/apache/xerces-c.git %XERCES_FOLDER%
 
 cd %XERCES_FOLDER%
 
